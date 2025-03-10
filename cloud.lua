cbc4565934759536933f7e7e929e4cf60db811b0
poisonhash <hone45.ugphone@gmail.com>
2025-03-10 12:54:33 -0400

    Update cloud.lua

--- cloud.lua
+++ cloud.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/poisonhash/cloud/refs/heads/main/cloud.lua",true))()
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <p>loadstring(game:HttpGet("https://raw.githubusercontent.com/poisonhash/cloud/refs/heads/main/cloud.lua",true))()</p>
+</body>
+</html>
